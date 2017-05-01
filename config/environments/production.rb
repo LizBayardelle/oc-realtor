a3f4256989af723f98717900a9c6afe11c8cc8ae
Liz Bayardelle <lizbayardelle@gmail.com>
2017-05-01 11:10:14 -0700

    Production Update for S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   # AWS
   config.paperclip_defaults = {
     storage: :s3,
+    s3_host_name: 's3-us-west-1.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
