37769fc65e05eedd5bd58fc52a02fff698b9c35b
Liz Bayardelle <lizbayardelle@gmail.com>
2018-07-15 17:00:20 -0700

    Most Recent ActiveRecord Issue

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -46,6 +46,7 @@ class BlogsController < ApplicationController
   def create
     @pillars = Pillar.all
     @blog = Blog.new(blog_params)
+    @blog.image.attach(params[:blog][:image])
     if current_user.id == 1
       @blog.user_id = 2
     else
@@ -84,7 +85,7 @@ class BlogsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def blog_params
-      params.require(:blog).permit(:title, :teaser, :body, :user_id, :tag_list, :link_text, :link_filename, :pillars_id, :image)
+      params.require(:blog).permit(:title, :teaser, :body, :user_id, :tag_list, :link_text, :link_filename, :pillars_id)
     end
 
 end
