e412eaefe06e11e154eb949156d2c4c251a14a17
Liz Bayardelle <lizbayardelle@gmail.com>
2018-07-18 07:06:42 -0700

    Production Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
     }
   }
 
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
