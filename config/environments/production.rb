c008cece4b43e8796de3094b045a6e87dc92cb8b
Liz Bayardelle <lizbayardelle@gmail.com>
2019-02-28 16:29:19 -0800

    Background Images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_files = true
+  config.assets.compile = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
