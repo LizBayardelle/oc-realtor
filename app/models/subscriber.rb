00fc18e5c1ebf62e61542cda868b64c3dd21785b
Liz Bayardelle <lizbayardelle@gmail.com>
2019-03-03 10:39:32 -0800

    Duplicate Subscribers Handled in Model

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,11 +1,17 @@
 class Subscriber < ApplicationRecord
   attr_accessor :tags_plain
   before_create :populate_tags
-  after_create :send_email
   after_create :send_goodies
+  after_create :check_for_duplicate_subscribers
+  after_create :send_email
 
 
   def send_email
+    Subscriber.all.each do |subscriber|
+      if subscriber.email == self.email
+        return
+      end
+    end
     NewSubscriberMailer.new_subscriber(self).deliver_now
   end
 
@@ -22,4 +28,15 @@ class Subscriber < ApplicationRecord
     return if self.tags_plain.blank?
     self.tags = self.tags_plain.split(",")
   end
+
+  def check_for_duplicate_subscribers
+    Subscriber.all.each do |subscriber|
+      if subscriber.email == self.email && self.tag_string != ""
+        subscriber.tag_string << ", " + self.tag_string
+        subscriber.save
+        self.delete
+      end
+    end
+  end
+
 end
